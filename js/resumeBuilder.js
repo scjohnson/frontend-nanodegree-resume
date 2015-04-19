80b9198c83a7fac7cc4ad6d4f3696129ad913046
Stephen Johnson <stephen.c.johnson@gmail.com>
2015-04-19 03:07:32 +0000

    finishing the first lesson

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,49 @@
-$("#main").append("Stephen C. Johnson");
+
+var bio = {
+    "name": "Stephen C. Johnson",
+    "role": "Developer",
+    "email" : "stephen.c.johnson@gmail.com",
+    "picture" : "images/fry.jpg",
+    "welcomeMessage" : "Welcome to my simple resume for learning JavaScript",
+    "skills" : ["C++, Java, Python, JavaScript"],
+};
+
+var work = {
+    "position": "Director of Space Programs",
+    "employer": "Stellar Science",
+    "years": 4,
+    "city": "Vienna, VA"
+};
+
+var education = [
+    {
+        "name": "Lawrence University",
+        "years": "1989-1993",
+        "city": "Appleton, WI",
+        "major": "Physics",
+        "degree": "BA"
+        
+    },
+    {
+        "name": "Stony Brook University",
+        "years": "1993-1997",
+        "city": "Stony Brook, NY",
+        "major": "Physics",
+        "degree": "PhD"
+        
+    }
+]
+
+var projects = [
+    "A special one"
+    ]
+
+
+var formattedName = HTMLheaderName.replace( "%data%", bio.name );
+var formattedRole = HTMLheaderRole.replace( "%data%", bio.role );
+
+$("#header").prepend( formattedRole );
+$("#header").prepend( formattedName );
+
+
+$("#main").append( work.position );
